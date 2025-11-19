e1c764d86513ed9527a9f32cd135056832299950
Manoj <duwaikoho.4hai@gmail.com>
2025-11-19 18:05:45 +0545

    gemini model upgraded

--- backend/controller/geminiMessageImageApi.js
+++ backend/controller/geminiMessageImageApi.js
@@ -20,7 +20,7 @@ const  geminiMessageImageApi = async (req, res) => {
         );
 
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
         // Default to an empty string if no text is provided
         const userInputText = req.body.text || ""; 
--- backend/controller/geminiTranslator.js
+++ backend/controller/geminiTranslator.js
@@ -6,7 +6,7 @@ const geminiTranslator = async (req, res) => {
     try {
         
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
         
         // Create a translation prompt based on the user message and target language
         const prompt = `Translate the following text from ${fromLanguage} to ${toLanguage} without additional explanations or context: "${inputText}"`;
